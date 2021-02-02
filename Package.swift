4887d13c79c74333677a6499ea1dd2f701358d14
Roderic Campbell <roderic@gmail.com>
2021-02-02 09:10:26 -0800

    Add Backtrace as a dependency

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
         .executable(name: "App", targets: ["MeridianWine"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/swift-server/swift-backtrace.git", from: "1.2.0"),
         .package(url: "https://github.com/rodericj/Meridian", from: "0.1.2"),
         .package(url: "https://github.com/GEOSwift/GEOSwift.git", from: "8.0.0"),
         .package(name: "SwiftgreSQL", url: "https://github.com/khanlou/SwiftgreSQL", from: "0.1.4"),
@@ -15,6 +16,7 @@ let package = Package(
         .target(name: "MeridianWine",
                 dependencies: [
                     "Meridian",
+                    .product(name: "Backtrace", package: "swift-backtrace"),
                     .product(name: "GEOSwift", package: "GEOSwift"),
                     .product(name: "SwiftgreSQL", package: "SwiftgreSQL"),
                 ],
