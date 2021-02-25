ac2592c89e3c66682aef564285b28056e40afff0
Roderic Campbell <roderic@gmail.com>
2021-02-25 07:04:33 -0800

    Move the header content out of the main func

--- Sources/MeridianWine/Routes/RegionHTML.swift
+++ Sources/MeridianWine/Routes/RegionHTML.swift
@@ -56,6 +56,69 @@ public struct GetRegionHTML: Responder {
             }
         }
     }
+    private var headerContent: Node {
+        return header {
+            script(src: "https://code.jquery.com/jquery-3.5.1.min.js")
+            script(src: "https://api.mapbox.com/mapbox.js/plugins/turf/v2.0.2/turf.min.js")
+            script(src: "https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.js")
+            "<link href='https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css' rel='stylesheet' />"
+            script(crossorigin: "anonymous",
+                   integrity: "sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi",
+                   src: "https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js")
+            script(src: "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js")
+            "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\">"
+            script(crossorigin: "anonymous",
+                   integrity: "sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG",
+                   src: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js")
+
+            script(src: "https://api.mapbox.com/mapbox.js/plugins/turf/v2.0.2/turf.min.js")
+            script {
+            """
+            function updateMap(regionUUID) {
+                console.log(regionUUID)
+                var url = '/region/' + regionUUID + '/geojson'
+                $.getJSON(url, function (geojson) {
+                    var bbox = turf.extent(geojson);
+                    var center = turf.center(geojson);
+                <!--        map.on('load', function () {-->
+
+                        map.addSource(regionUUID, {
+                            'type': 'geojson',
+                            'data': geojson
+                        });
+                        map.addLayer({
+                            'id': regionUUID + 'regionSourceLayer',
+                            'type': 'fill',
+                            'source': regionUUID,
+                            'layout': {},
+                            'paint': {
+                                'fill-color': '#088',
+                                'fill-opacity': 0.8
+                            }
+                        });
+                        
+                        map.fitBounds(bbox, {padding:20});
+                <!--        });    -->
+
+
+                });
+            }
+            """
+            }
+            style {
+                """
+                    body { margin: 0; padding: 0; }
+                    #map { position: absolute; top: 0; bottom: 0; width: 100%; }
+                """
+            }
+
+            h1 {
+                "Wine Regions"
+            }
+        }
+
+    }
+    
     public func execute() throws -> Response {
         let regionsResponse = try database.fetchAllRegions()
         let regions: [Region]
@@ -67,65 +130,7 @@ public struct GetRegionHTML: Responder {
         }
         return html(lang: "en-US") {
             title{ "Wine Regions" }
-            header {
-                script(src: "https://code.jquery.com/jquery-3.5.1.min.js")
-                script(src: "https://api.mapbox.com/mapbox.js/plugins/turf/v2.0.2/turf.min.js")
-                script(src: "https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.js")
-                "<link href='https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css' rel='stylesheet' />"
-                script(crossorigin: "anonymous",
-                       integrity: "sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi",
-                       src: "https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js")
-                script(src: "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js")
-                "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\">"
-                script(crossorigin: "anonymous",
-                       integrity: "sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG",
-                       src: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js")
-
-                script(src: "https://api.mapbox.com/mapbox.js/plugins/turf/v2.0.2/turf.min.js")
-                script {
-                """
-                function updateMap(regionUUID) {
-                    console.log(regionUUID)
-                    var url = '/region/' + regionUUID + '/geojson'
-                    $.getJSON(url, function (geojson) {
-                        var bbox = turf.extent(geojson);
-                        var center = turf.center(geojson);
-                    <!--        map.on('load', function () {-->
-
-                            map.addSource(regionUUID, {
-                                'type': 'geojson',
-                                'data': geojson
-                            });
-                            map.addLayer({
-                                'id': regionUUID + 'regionSourceLayer',
-                                'type': 'fill',
-                                'source': regionUUID,
-                                'layout': {},
-                                'paint': {
-                                    'fill-color': '#088',
-                                    'fill-opacity': 0.8
-                                }
-                            });
-                            
-                            map.fitBounds(bbox, {padding:20});
-                    <!--        });    -->
-
-
-                    });
-                }
-                """
-                }
-                style {
-                    """
-                        body { margin: 0; padding: 0; }
-                        #map { position: absolute; top: 0; bottom: 0; width: 100%; }
-                    """
-                }
- 
-                h1 {
-                    "Wine Regions"
-                }
-            }
+            headerContent
             body {
                 div(class: "row") {
                     div(class: "col-md-3") {
